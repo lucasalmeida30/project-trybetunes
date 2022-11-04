57b3b0fc120eb2a164556be78a599429e4ca11fc
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-11-03 22:49:39 -0300

    adiciona requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,39 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artisti: '',
+    isDisabled: true,
+  };
+
+  handleInput = (event) => {
+    const { artisti } = this.state;
+    this.setState(({
+      artisti: event.target.value,
+      isDisabled: artisti.length < 1,
+    }));
+  };
+
   render() {
+    const { artisti, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          onChange={ this.handleInput }
+          value={ artisti }
+          type="text"
+          placeholder="Nome do artista"
+          data-testid="search-artist-input"
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ isDisabled }
+          // onClick={ bil }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
