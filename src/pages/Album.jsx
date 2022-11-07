890db32deaba5fc4a1457949a7cb957e03b0f67d
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-11-07 12:12:58 -0300

    erros lint

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -48,7 +48,11 @@ class Album extends React.Component {
 }
 
 Album.propTypes = {
-  match: PropTypes.shape().isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default Album;
